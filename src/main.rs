0d9a3fe38576e77fd299c3d462d50475414eff6d
Asadul Al Galib <asadulgalibvat69@gmail.com>
2024-07-13 17:18:36 +0600

    feat: alpha release

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,19 @@ mod network;
 mod model;
 mod util;
 
-use std::io::{stdin, stdout, Write};
 use std::process::Command;
 use std::error::Error;
 use fuzzypicker::FuzzyPicker;
-//use reqwest::blocking::Client;
-//use reqwest::header::{HeaderMap, HeaderValue, REFERER};
 use crate::network::Network;
-
+use crate::util::sanitize_filename;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut stdout = stdout();
-    let stdin = stdin();
-
-    stdout.write(b"Search Anime: ")?;
-    stdout.flush()?;
-
-    let mut anime_name = String::new();
-    stdin.read_line(&mut anime_name)?;
-    anime_name = String::from(anime_name.trim());
+    let config = util::parse_args()?;
+    if !config.has_task { return Ok(()); }
+    
+    let anime_name = util::user_input(
+	&format!("{}Search Anime: {}", util::COLOR_CYAN_BOLD, util::COLOR_RESET)
+    )?;
     println!("Searching for \"{anime_name}\"...");
 
     let network = Network::new()?;
@@ -28,87 +22,148 @@ fn main() -> Result<(), Box<dyn Error>> {
 	&network.search_anime(&anime_name)?
     )?;
     let anime_list = json.get_anime_list();
-    let mut video_url_list;
-    if !anime_list.is_empty() {
-	// select anime from the search results
-	let mut anime_picker = FuzzyPicker::new(&anime_list);
-	let selected_anime = anime_picker.pick()?;
-	if let Some(anime) = selected_anime {
-	    // select episode from the episode list
-	    let episode_list = anime.get_episodes_list();
-	    let mut episode_picker = FuzzyPicker::new(&episode_list);
-	    let selected_episode = episode_picker.pick()?;
-	    if let Some(episode_no) = selected_episode {
-		let total = episode_list.len();
-		let mut index = episode_list.iter().position(|s| s.as_str() == episode_no.as_str()).unwrap();
-		let mut quality = "360";
-		let commands = vec!["replay", "next", "previous", "change quality", "exit"];
-		let qualities = vec!["360", "480", "720", "1080"];
-		let mut command_picker = FuzzyPicker::new(&commands);
-		loop {
-		    video_url_list = Vec::new();
-		    // get source urls for the selected episode
-		    let json: serde_json::Value = serde_json::from_str(
-			&network.get_sources(&anime.id, "sub", &episode_list[index])?
-		    )?;	
-		    let sources = json["data"]["episode"]["sourceUrls"].as_array().unwrap();
-		    for source in sources {
-			let source_name = source["sourceName"].as_str().unwrap();
-			match source_name {
-			    "Luf-mp4" => { //| "Sak" | "Kir" | "S-mp4"
-				let source_url = &source["sourceUrl"].as_str().unwrap()[2..];
-				let provider_id = util::decode_provider_id(source_url);
-				let json: serde_json::Value = serde_json::from_str(
-				    &network.get_links(&provider_id)?
-				)?;
-				let links = json["links"].as_array().unwrap();
-				for link in links {
-				    let video_url = link["link"].as_str().unwrap();
-				    if video_url.ends_with(".m3u8") {
-					video_url_list.push(video_url.replace(".m3u8", format!(".{}.m3u8", quality).as_str()));
-				    }
-				}
-			    },
-			    _ => {}
-			}
-		    }
-		    Command::new("mpv")
-			.arg(&video_url_list[0])
-			.output()
-			.expect("mpv command failed to start");
-		    let selected_command = command_picker.pick()?;
-		    if let Some(command) = selected_command {
-			match command {
-			    "exit" => break,
-			    "next" => {
-				if index < total-1 { index += 1; }
-				else { break; }
-			    },
-			    "previous" => {
-				if index > 0 { index -= 1; }
-				else { break; }
-			    },
-			    "change quality" => {
-				let mut quality_picker = FuzzyPicker::new(&qualities);
-				let selected_quality = quality_picker.pick()?;
-				if selected_quality != None {
-				    quality = selected_quality.unwrap();
-				}
-			    },
-			    _ => {}
-			}
-		    }
-		}
-		
-	    } else {
-		println!("Selection cancelled.")
-	    }
+    // exit if no anime found
+    if anime_list.is_empty() { return Err("No Anime Found.".into()); }
+    
+    // select anime from the search results
+    let mut anime_picker = FuzzyPicker::new(&anime_list);
+    let selected_anime = anime_picker.pick()?;
+    if selected_anime.is_none() { return Err("No Anime Selected.".into()); }
+    let anime = selected_anime.unwrap();
+
+    let mut quality = match config.quality {
+	360 => "360",
+	480 => "480",
+	720 => "720",
+	_ => "1080"
+    };
+
+    let episode_list = anime.get_episodes_list();
+    let mut video_url_list: Vec<String>;
+    if config.download {
+	let (start, end);
+	if config.range_start == 0 {
+	    start = String::from(&episode_list[0]);
 	} else {
-	    println!("Selection cancelled.")
+	    start = config.range_start.to_string();
 	}
-    } else {
-	println!("No anime found!");
+	if config.range_end == 0 || config.range_end as usize > episode_list.len() { 
+	    end = String::from(&episode_list[episode_list.len()-1]); 
+	} else {
+	    end = config.range_end.to_string();
+	}
+	let start_index = episode_list.iter().position(|s| s.as_str() == start.as_str()).unwrap();
+	let end_index = episode_list.iter().position(|s| s.as_str() == end.as_str()).unwrap();
+
+	for episode in &episode_list[start_index..end_index+1] {
+	    let video_title = format!("{} - Episode {}", anime.name, episode);
+	    println!(
+		"{}Downloading {}{}", 
+		util::COLOR_GREEN_BOLD, 
+		video_title,
+		util::COLOR_RESET
+	    );
+	    let filename = format!("{}.mp4", sanitize_filename(&video_title));
+	    video_url_list = get_video_url_list(&network, &anime.id, &episode, &quality)?;
+	    let mut cmd = Command::new("yt-dlp")
+		.arg(&video_url_list[0])
+		.arg("--no-skip-unavailable-fragments")
+		.arg("--fragment-retries")
+		.arg("infinite")
+		.arg("-N")
+		.arg("16")
+		.arg("-o")
+		.arg(&filename)
+		.spawn()
+		.expect("yt-dlp command failed to start");
+	    cmd.wait()?;
+	}
+	
+	return Ok(());
     }
     
+    // select episode from the episode list
+    let mut episode_picker = FuzzyPicker::new(&episode_list);
+    let mut selected_episode = episode_picker.pick()?;
+    if selected_episode.is_none() { return Err("No Episode Selected.".into()); }
+    let mut episode_no = selected_episode.unwrap();
+    
+    let total = episode_list.len();
+    let mut index = episode_list.iter().position(|s| s.as_str() == episode_no.as_str()).unwrap();
+    let commands = vec!["replay", "next", "previous", "select episode", "change quality", "exit"];
+    let qualities = vec!["360", "480", "720", "1080"];
+    let mut command_picker = FuzzyPicker::new(&commands);
+    let mut quality_picker = FuzzyPicker::new(&qualities);
+    loop {
+	let video_title = format!("{} - Episode {}", anime.name, episode_list[index]);
+	video_url_list = get_video_url_list(&network, &anime.id, &episode_list[index], &quality)?;
+	//if config.download {
+	//    Command::new("yt-dlp")
+	//	.arg(&video_url_list[0])
+	//}
+	Command::new("mpv")
+	    .arg(format!("--force-media-title={}", video_title))
+	    .arg(&video_url_list[0])
+	    .output()
+	    .expect("mpv command failed to start");
+	let selected_command = command_picker.pick()?;
+	if selected_command.is_none() { break; }
+	let command = selected_command.unwrap();
+	match command {
+	    "exit" => break,
+	    "next" => {
+		if index < total-1 { index += 1; }
+		else { break; }
+	    },
+	    "previous" => {
+		if index > 0 { index -= 1; }
+		else { break; }
+	    },
+	    "select episode" => {
+		selected_episode = episode_picker.pick()?;
+		if selected_episode.is_none() { return Err("No Episode Selected.".into()); }
+		episode_no = selected_episode.unwrap();
+		index = episode_list.iter().position(|s| s.as_str() == episode_no.as_str()).unwrap();
+	    }
+	    "change quality" => {
+		let selected_quality = quality_picker.pick()?;
+		if selected_quality != None {
+		    quality = selected_quality.unwrap();
+		}
+	    },
+	    _ => {}
+	}
+    }
     Ok(())
+}
+
+fn get_video_url_list
+(network: &Network, id: &str, episode_no: &str, quality: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let mut video_url_list = Vec::new();
+    // get source urls for the selected episode
+    let json: serde_json::Value = serde_json::from_str(
+	&network.get_sources(id, "sub", episode_no)?
+    )?;	
+    let sources = json["data"]["episode"]["sourceUrls"].as_array().unwrap();
+    for source in sources {
+	let source_name = source["sourceName"].as_str().unwrap();
+	match source_name {
+	    "Luf-mp4" => { //| "Sak" | "Kir" | "S-mp4"
+		let source_url = &source["sourceUrl"].as_str().unwrap()[2..];
+		let provider_id = util::decode_provider_id(source_url);
+		let json: serde_json::Value = serde_json::from_str(
+		    &network.get_links(&provider_id)?
+		)?;
+		let links = json["links"].as_array().unwrap();
+		for link in links {
+		    let video_url = link["link"].as_str().unwrap();
+		    if video_url.ends_with(".m3u8") {
+			video_url_list.push(video_url.replace(".m3u8", format!(".{}.m3u8", quality).as_str()));
+		    }
+		}
+	    },
+	    _ => {}
+	}
+    }
+    Ok(video_url_list)
 }
--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,145 @@
+use std::io::{stdin, stdout, Write};
+use std::env::args;
+use std::path::Path;
+use std::error::Error;
+
+const VERSION: &str = "0.1.0";
+pub const COLOR_RED_BOLD: &str = "\x1b[1;31m";
+pub const COLOR_GREEN_BOLD: &str = "\x1b[1;32m";
+pub const COLOR_CYAN_BOLD: &str = "\x1b[1;36m";
+pub const COLOR_RESET: &str = "\x1b[0m";
+
+#[derive(Debug)]
+pub struct Config {
+    pub has_task: bool,
+    pub download: bool,
+    pub quality: u32,
+    pub range_start: u32,
+    pub range_end: u32,
+}
+
+pub fn parse_args() -> Result<Config, Box<dyn Error>> {
+    let mut config = Config {
+        has_task: true,
+        download: false,
+        quality: 720,
+        range_start: 0,
+        range_end: 0
+    };
+    let mut arg;
+    let argv: Vec<String> = args().collect();
+    let argc = argv.len();
+    let program_name = Path::new(&argv[0])
+        .file_stem().unwrap().to_str().unwrap();
+    let mut counter = 1;
+    loop {
+        if argc == counter { break; }
+        arg = argv[counter].as_str();
+        match arg {
+            "-h" | "--help" => {
+                config.has_task = false;
+                print_usage(&program_name);
+                break;
+            },
+            "-V" | "--version" => {
+                config.has_task = false;
+                println!("{program_name} {VERSION}");
+                break;
+            },
+            "-d" | "--download" => config.download = true,
+            "-q" | "--quality" => {
+                if argc > counter+1 {
+                    counter += 1;
+                    let quality = argv[counter].as_str();
+                    match quality {
+                        "360" | "480" | "720" | "1080" => config.quality = quality.parse()?,
+                        _ => {
+                            config.has_task = false;
+                            error_msg("Invalid quality.");
+                            print_usage(&program_name);
+                        }
+                    }
+                }
+            },
+            "-r" | "--range" => {
+                if argc > counter+1 {
+                    counter += 1;
+                    let range: Vec<_> = argv[counter].split('-').collect();
+                    if range.len() == 2 {
+                        config.range_start = range[0].parse()?;
+                        config.range_end = range[1].parse()?;
+                    } else {
+                        error_msg("Invalid format for range.");
+                        print_usage(&program_name);
+                    }
+                } else {
+                    config.has_task = false;
+                    error_msg("Invalid format for range.");
+                    print_usage(&program_name);
+                }
+            },
+            x => {
+                error_msg(&format!("Unknown option \"{x}\".")); 
+                print_usage(&program_name);
+            }
+        }
+        counter += 1;
+    }
+    Ok(config)
+}
+
+fn error_msg(msg: &str) {
+    eprintln!("{COLOR_RED_BOLD}{}{COLOR_RESET}\n", msg);
+}
+
+fn print_usage(program_name: &str) {
+    let usage = format!(r#"Description:
+  CLI tool to stream and download Anime
+
+Usage: 
+  {program_name} [OPTIONS]
+
+Options:
+  -h, --help           Print help
+  -V, --version        Print version
+  -d, --download       Download anime
+  -q, --quality        Specify quality
+                       Available qualities are 360, 480, 720 & 1080
+  -r, --range          Specify episode range separated by dash(-), ranges are inclusive"#);
+    println!("{}", usage);
+}
+
+pub fn user_input(prompt: &str) -> Result<String, Box<dyn Error>> {
+    stdout().write(prompt.as_bytes())?;
+    stdout().flush()?;
+    let mut anime_name = String::new();
+    stdin().read_line(&mut anime_name)?;
+    Ok(anime_name.trim().to_string())
+}
+
+pub fn sanitize_filename(filename: &str) -> String {
+    let illegal_chars = [' ', '/', '\\', '?', '%', '*', ':', '|', '"', '<', '>', '.'];
+
+    let sanitized = filename.chars()
+        .map(|c| if illegal_chars.contains(&c) { '_' } else { c })
+        .collect::<String>();
+
+    // Remove control characters (ASCII 0-31) and Unicode replacement character (U+FFFD)
+    let sanitized = sanitized.chars()
+        .filter(|c| !c.is_control())
+        .collect::<String>();
+
+    // Trim any leading or trailing whitespace and periods
+    let sanitized = sanitized.trim_start_matches('.').trim_end_matches('.').trim();
+
+    // Ensure the filename is not empty after trimming
+    if sanitized.is_empty() {
+        return String::from("unnamed_file");
+    }
+
+    sanitized.to_lowercase()
+}
+
 pub fn decode_provider_id(resp: &str) -> String {
     let chars = resp.chars().collect::<Vec<char>>();
     let mut output = String::new();
